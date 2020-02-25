cdbc10ba7eadd11477c2510f591de654f24c369f
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-02-25 16:39:19 -0500

    able to successfully fetch friends from api, and display them to the page under the protected route

--- friends/src/components/FriendsListPrivate.js
+++ friends/src/components/FriendsListPrivate.js
@@ -1,16 +1,39 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const FriendsListPrivate = (props) => {
 
     const [friendsList, setFriendsList] = useState({friends: []})
 
-    const getData = () => {
+    useEffect(() => {
+        getData();
+    }, [])
 
+    const getData = () => {
+        const token = window.localStorage.getItem('token');
+        axiosWithAuth()
+            .get('/api/friends')
+            .then(response => {
+                console.log("friends: ", response)
+                setFriendsList({
+                    friends: response.data
+                })
+            })
+            .catch(error => {
+                console.log("Error with GET request to fetch list of friends: ", error)
+            })
     }
 
     return (
         <div className="FriendsList">
             <h1>Friends List</h1>
+            <ul>
+                {friendsList.friends.map(friend => {
+                    return(
+                        <li>{friend.name}</li>
+                    )
+                })}
+            </ul>
         </div>
     );
 }
